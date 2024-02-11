0f399b96980fd30e4e80a441b78cc880824b123b
momo-41 <minimizu41@gmail.com>
2024-02-11 23:21:56 +0900

    Subheaderの作成

---
+++ app/components/Subheader.tsx
@@ -0,0 +1,35 @@
+import * as React from "react";
+import { Box, Typography } from "@mui/material";
+
+const Subheader = () => {
+  return (
+    <Box
+      height={"70vh"}
+      sx={{
+        backgroundImage: `url("/image/engineer-03.jpg")`,
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+      }}
+    >
+      <Box height={"100%"} sx={{ background: "rgba(255, 255, 255, 0.65) " }}>
+        <Box
+          display={"flex"}
+          flexDirection={"column"}
+          alignItems={"center"}
+          height={"100%"}
+          paddingY={19}
+        >
+          <Typography fontSize={{ xs: 25, md: 35 }}>
+            エンジニアのための就活対策
+          </Typography>
+          <Typography fontSize={{ xs: 25, md: 35 }}>
+            エンジニアのための就活対策
+          </Typography>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default Subheader;
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
+import Subheader from "./components/Subheader";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Header />
+        <Subheader />
         {children}
       </body>
     </html>
