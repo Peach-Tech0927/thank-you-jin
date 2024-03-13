664c417bc201f706bd5d8ec325fa1409b752c1d2
momo-41 <minimizu41@gmail.com>
2024-03-13 14:19:18 +0900

    不要なインポートの削除

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
-import Subheader from "./components/Subheader";
-import Tabpanel from "./components/Tabpanel";
 
 const inter = Inter({ subsets: ["latin"] });
 
