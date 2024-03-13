bbf2ff537e6b1da57156f9859a1dfdc29c3b8645
momo-41 <minimizu41@gmail.com>
2024-03-13 14:13:25 +0900

    letterSpacingの追加とborderRadiusの修正

--- app/components/GridCard.tsx
+++ app/components/GridCard.tsx
@@ -11,14 +11,14 @@ const GridCard = () => {
     >
       {messages.map((message, index) => (
         <Grid key={index} item xs={12} md={6}>
-          <Box mx={1} mt={1} bgcolor={"#F8E0F1"} borderRadius={3}>
+          <Box mx={1} mt={1} bgcolor={"#F8E0F1"} borderRadius={5} boxShadow={5}>
             <Box
               px={{ xs: 3, sm: 5 }}
               py={{ xs: 2, sm: 4 }}
               fontSize={{ xs: "13px", sm: "16px" }}
             >
               <Typography marginBottom={1}>{message.name}</Typography>
-              <Typography>{message.message}</Typography>
+              <Typography letterSpacing={0.8}>{message.message}</Typography>
             </Box>
           </Box>
         </Grid>
--- app/components/YearGridCard.tsx
+++ app/components/YearGridCard.tsx
@@ -19,14 +19,20 @@ const YearGridCard: React.FC<TProps> = ({ yearProps, cardColor }) => {
         .filter((message) => yearProps === message.year)
         .map((message, index) => (
           <Grid key={index} item xs={12} md={6}>
-            <Box mx={1} mt={1} bgcolor={cardColor} borderRadius={3}>
+            <Box
+              mx={1}
+              mt={1}
+              bgcolor={cardColor}
+              borderRadius={5}
+              boxShadow={5}
+            >
               <Box
                 px={{ xs: 3, sm: 5 }}
                 py={{ xs: 2, sm: 4 }}
                 fontSize={{ xs: "13px", sm: "16px" }}
               >
                 <Typography marginBottom={1}>{message.name}</Typography>
-                <Typography>{message.message}</Typography>
+                <Typography letterSpacing={0.8}>{message.message}</Typography>
               </Box>
             </Box>
           </Grid>
