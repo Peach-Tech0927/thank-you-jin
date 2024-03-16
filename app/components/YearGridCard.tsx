1f092c8e4025d7c7732d5404732c7dbe1c3e2536
momo-41 <minimizu41@gmail.com>
2024-03-16 18:52:18 +0900

    カードに桜装飾の追加

--- app/components/YearGridCard.tsx
+++ app/components/YearGridCard.tsx
@@ -1,5 +1,6 @@
-import { Grid, Box, Typography } from "@mui/material";
+import { Grid, Box, Typography, styled } from "@mui/material";
 import { messages } from "../data/data";
+import Image from "next/image";
 
 type TProps = {
   yearProps: string;
@@ -24,14 +25,34 @@ const YearGridCard: React.FC<TProps> = ({ yearProps, cardColor }) => {
               bgcolor={cardColor}
               borderRadius={5}
               boxShadow={5}
+              position={"relative"}
             >
-              <Box
-                px={{ xs: 3, sm: 5 }}
-                py={{ xs: 2, sm: 4 }}
-                fontSize={{ xs: "13px", sm: "16px" }}
-              >
-                <Typography marginBottom={1}>{message.name}</Typography>
-                <Typography letterSpacing={0.8}>{message.message}</Typography>
+              <Box px={{ xs: 3, sm: 5 }} py={{ xs: 2, sm: 4 }}>
+                <Box position={"absolute"} right={-10} top={-10}>
+                  <StyledImage
+                    src={"/sakura.png"}
+                    alt={""}
+                    width={80}
+                    height={80}
+                  />
+                </Box>
+                <Typography
+                  mt={{ xs: 0.7, sm: 1 }}
+                  mb={{ xs: 0.5, sm: 0.7 }}
+                  letterSpacing={0.8}
+                  fontWeight={500}
+                  fontSize={{ xs: "14px", sm: "17px" }}
+                  fontFamily={"serif"}
+                >
+                  {message.name}
+                </Typography>
+                <Typography
+                  fontSize={{ xs: "13px", sm: "16px" }}
+                  letterSpacing={0.8}
+                  fontFamily={"serif"}
+                >
+                  {message.message}
+                </Typography>
               </Box>
             </Box>
           </Grid>
@@ -39,5 +60,12 @@ const YearGridCard: React.FC<TProps> = ({ yearProps, cardColor }) => {
     </Grid>
   );
 };
+
+const StyledImage = styled(Image)(({ theme }) => ({
+  [theme.breakpoints.down("md")]: {
+    width: 50,
+    height: 50,
+  },
+}));
 
 export default YearGridCard;
